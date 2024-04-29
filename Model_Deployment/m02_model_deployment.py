be6f4faa64dda651e7398cf7ab45a246694cb2d8
ngarzonr1 <147946672+ngarzonr1@users.noreply.github.com>
2024-04-28 23:02:22 -0500

    Add files via upload

---
+++ Model_Deployment/m02_model_deployment.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import pandas as pd
+import joblib
+import sys
+import os
+
+def predict_price(mileage):
+
+    clf = joblib.load(os.path.dirname(__file__) + '/pricing_clf.pkl') 
+
+    arr = np.array([mileage])
+    arr = arr.reshape(1,-1)
+
+    # Make prediction
+    p1 = clf.predict_price(arr)[0]
+
+    return p1
+
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) == 1:
+        print('Please add a Mileage')
+        
+    else:
+
+        mil = sys.argv[1]
+
+        p1 = predict_proba(mil)
+        
+        print(mil)
+        print('Price predicted: ', p1)
+        
